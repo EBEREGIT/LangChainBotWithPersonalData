60a214c4fd50fa7e12b5e426f9b20b84c2cbcf1e
eberegit <samsoneberenjoku@gmail.com>
2024-04-05 13:42:18 +0100

    updates

--- frontend/src/components/Btn.jsx
+++ frontend/src/components/Btn.jsx
@@ -1,12 +1,15 @@
 /* eslint-disable react/prop-types */
 import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
+import { Variable } from "../context/Variable";
 
 export default function Btn({ brand, label }) {
   const { startListening } = useContext(AppContext);
+  const { isListening, working } = useContext(Variable);
 
   return (
     <button
+      disabled={isListening || working}
       onClick={() => {
         startListening(brand);
       }}
